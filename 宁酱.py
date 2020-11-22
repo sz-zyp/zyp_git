7eef87310cc02528e07e54a641d2e8fbe514c56d
suozhuo-hp <824147110@qq.com>
2020-11-22 22:12:59 +0800

    添加了周末大作业注释

--- 宁酱.py
+++ 宁酱.py
@@ -95,19 +95,45 @@ def Day8():
     # 读hhh.txt文件
     # 生成以下字典
     # {'A': ['c', 'a', 'b', 'd'], 'B': ['a', 'd'], 'C': ['a', 'e']}
-    import re
-    A_dic = {}
-    with open("hhh.txt") as f1:
-        for x in f1:
-            x = re.sub("\n", '', x)  # 去除行尾符
-            save_split = re.split("\t", x)
+    import re  # 调用re模块，这是个正则表达式的包
+    A_dic = {}  # 定义一个空的字典
+    with open("hhh.txt", mode='r', encoding='utf-8') as f1:
+        # 以with open 这种方式打开文件，这种方式相比你的那种方式最大的优点就是不用关
+        # mode是选择读写方式，encoding是选择编码方式
+        for x in f1:  # 按行读取，注意这很重要，因为我们处理的经常是很大的文件，我们不可能把所有的文件全都放到内存里，所以一行一行读取，是一种很好的方式。
+            x = re.sub("\n", '', x)  # 去除行尾符。用到的是re包中的sub替换功能，把\n替换为了空
+            save_split = re.split("\t", x)  # 把这行文件按照\t切割，并把切割后的结果赋值给了列表save_split
             A_dic.setdefault(save_split[0], set()).add(save_split[1])   # 学会setdefault函数
+            """
+            字典名称.setdefault()方法
+            
+            字典名称.setdefault(key名称, set()).add(value名称)
+            （1）这个方法的功能是，如果这个字典中没有这个key名称，那么就在这个字典中添加上这个key，并把这个key对应的value设置成set() {ps:set()是集合的赋空方式}，
+            然后再把value名称添加到空集合中 {ps .add()是往集合里添加元素，类似于.append()往列表里添加元素}
+            (2)如果这个字典中有这个key名称,则直接在这个key对应的value上添加上后面的value名称这个值。
+            
+            那么为什么要把，字典的value的类型设置成集合呢？
+            答：为了去重
+            
+            课后思考？
+            字典名称.setdefault(key名称, []).append(value名称)的作用?
+            课后拓展
+            字典名称.setdefault(key名称, {}).setdefault(名称, set()).add(value名称)
+            """
+
+    # 上面的代码生成了{'A': {'c', 'a', 'b', 'd'}, 'B': {'a', 'd'}, 'C': {'a', 'e'}}的结果，字典的value是集合，下面的代码是把字典的value改为列表。
     for dict_item in A_dic:  # 集合转列表
         A_dic[dict_item] = list(A_dic[dict_item])
     print(A_dic)
 # 下周计划
 # 学习csv文件的读写（用csv模块）
 # 学习json文件的读写(用json模块)
+
+
+def Day9():
+    # 明天再有题目，没想好                       读取Day9.csv文件，并把里面的内容存成字典结构
+    pass
+
 
 
 
